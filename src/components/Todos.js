e46cb9a06630d4cbc5d4016ed2892c1157223e93
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-04-26 17:40:45 +0200

    Up todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -12,22 +12,22 @@ const initialTodos = [
   },
   {
     text: "Jouer avec le <title></title> 🤩",
-    isCompleted: false,
+    isCompleted: true,
     id: "efc6331d-7ca2-49a6-b014-378b8280b33d",
   },
   {
     text: "Enregistrer les tâches dans localStorage 🤓",
-    isCompleted: false,
+    isCompleted: true,
     id: "9e60d353-cd72-40bb-97e6-5841e51635c0",
   },
   {
     text: "Mettre en place dark mode 😎",
-    isCompleted: false,
+    isCompleted: true,
     id: "df0ce18c-b4fa-4651-82c0-72fad6b486e4",
   },
   {
     text: "Enregistrer dark mode dans localStorage 🥳",
-    isCompleted: false,
+    isCompleted: true,
     id: "206e8742-02b1-4ce9-92d2-d6184588f4c3",
   },
 ];
